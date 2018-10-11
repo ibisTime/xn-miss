9545ae5e71790c3949295d5a5fd68df10bb137bd
tao <taojian@192.168.1.186>
2018-10-11 20:08:08 +0800

    脚本整理，系统部分测试

--- src/main/java/com/ogc/standard/dto/req/XN630507Req.java
+++ src/main/java/com/ogc/standard/dto/req/XN630507Req.java
@@ -1,8 +1,11 @@
 package com.ogc.standard.dto.req;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 public class XN630507Req {
 
     // 编号（选填）
+    @NotBlank
     private String code;
 
     public String getCode() {
